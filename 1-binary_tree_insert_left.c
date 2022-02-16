20a0138433a8cb0bafbeaaf6453c9de66b056278
birhanus <birhanushimles716@gmail.com>
2022-02-16 18:30:54 +0000

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,7 +32,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		NewNode->left = parent->left;
 		parent->left->parent = NewNode;
 
-		return (NewNode);
 	}
 
 	parent->left = NewNode;
