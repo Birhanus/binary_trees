3a669b890ff8105f7137fc81a0cd824e3e9e49b4
birhanus <birhanushimles716@gmail.com>
2022-02-16 14:20:17 +0000

    only create new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,62 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "binary_trees.h"
+/**
+  *binary_tree_node - create new node for tree
+  *@parent: parent of the created node
+  *@value: the value to be inserted on the node
+  *Return: ponter of the created node or Null on fail
+  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *NewNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	binary_tree_t *currentNode;
+	binary_tree_t *NewNode;
+
+	NewNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (NewNode == NULL)
+	{
+		return (NULL);
+	}
 
 	NewNode->n = value;
-        NewNode->left = NULL;
+	NewNode->left = NULL;
 	NewNode->right = NULL;
+	NewNode->parent = parent;
 
-	if(parent == NULL)
-	{
-		currentNode = NewNode;
-		NewNode->parent = NULL;
-		return (NewNode);
-	}
-	else
-	{
-		NewNode->parent = parent;
-		currentNode = parent;
-
-		while(1)
-		{
-			//go to left of the tree
-			if (value < parent->n)
-			{
-				currentNode = currentNode->left;
-
-				//insert to the left
-				if(currentNode == NULL)
-				{
-					parent->left = NewNode;
-					return (NewNode);
-				}
-
-			}
-
-			// go to right of the tree
-			else
-			{
-				currentNode = currentNode->right;
-
-				//inserr to the right
-				if(currentNode == NULL)
-				{
-					parent->right = NewNode;
-					return (NewNode);
-				}
-
-			}
-
-
-		}
-	}
-
-
-
-
+	return (NewNode);
+	
 }
