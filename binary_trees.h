6d7c19d869708ba4f647197fcd5f25d52e8dafda
birhanus <birhanushimles716@gmail.com>
2022-02-17 15:23:15 +0000

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,12 @@
 #ifndef _binary_trees_h_
 #define _binary_trees_h_
 
+#include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -29,4 +34,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif
